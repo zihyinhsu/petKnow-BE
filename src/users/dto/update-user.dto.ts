4a9e5fabf0524c3d5dc6d997f145f0a565b71345
zyhsu <jenny010328@gmail.com>
2023-11-25 10:36:10 +0800

    add : update-user-dto

---
+++ src/users/dto/update-user.dto.ts
@@ -0,0 +1,8 @@
+import { userDto } from './user.dto';
+import { PickType } from '@nestjs/swagger'; // PartialType ：局部性套用
+
+// 使用mapped-type 映射型別來重用DTO
+export class updateUserDto extends PickType(userDto, [
+  'name',
+  'introduction',
+]) {}
