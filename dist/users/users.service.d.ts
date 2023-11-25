6e5da672e47b41f2648e6ba7d701ccbf3378f1af
zyhsu <jenny010328@gmail.com>
2023-11-25 11:01:58 +0800

    Remove dist from version control

--- dist/users/users.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { User } from './user.entity';
-import { Repository } from 'typeorm';
-export declare class UsersService {
-    private repo;
-    constructor(repo: Repository<User>);
-    findOne(query: any): Promise<User>;
-}
