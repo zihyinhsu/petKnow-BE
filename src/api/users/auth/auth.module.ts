de3e5e308ffe17f739a3171169a0b1677a3b8bce
Benson <a283835639@gmail.com>
2024-06-03 20:45:09 +0800

    Update: 移除無用註解
    Related Work Item:
    
    # 如主旨

--- src/api/users/auth/auth.module.ts
+++ src/api/users/auth/auth.module.ts
@@ -12,7 +12,6 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
 import { AuthController } from './auth.controller';
 import { GoogleOauthStrategy } from './google.strategy';
-// import { UserService } from 'api2/user/user.service';
 import { Api2Module } from 'api2/api2.module';
 
 @Module({
