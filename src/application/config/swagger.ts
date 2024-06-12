7cf0574f65822a020174b4f09a9814196b830a44
Benson <a283835639@gmail.com>
2024-06-12 13:15:45 +0800

    create: 產生 swagger.json

--- src/application/config/swagger.ts
+++ src/application/config/swagger.ts
@@ -1,5 +1,7 @@
 import { DocumentBuilder, SwaggerModule, SwaggerCustomOptions } from '@nestjs/swagger';
 import { INestApplication } from '@nestjs/common';
+import { writeFileSync } from 'fs';
+import path from 'path';
 
 export const swaggerPrefix = 'api';
 
@@ -23,6 +25,14 @@ export function setupSwagger(app: INestApplication): void {
   const options: SwaggerCustomOptions = {
     explorer: true, // 開啟搜尋列
   };
+
+  // 寫入檔案
+  const filePath = path.join(__dirname, 'swagger_output.json');
+
+  // 產生 JSON 格式的 Swagger 文件
+  const swaggerJson = JSON.stringify(document, null, 2);
+
+  writeFileSync(filePath, swaggerJson, 'utf-8');
 
   SwaggerModule.setup(swaggerPrefix, app, document, options);
 }
