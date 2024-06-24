cc06099a9ca49b9c973f0996375c9450af23ccad
Benson <a283835639@gmail.com>
2024-06-24 18:09:36 +0800

    update: unit test

--- src/api/backstage/backstage.service.spec.ts
+++ src/api/backstage/backstage.service.spec.ts
@@ -1,14 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BackstageService } from './backstage.service';
-import { DatabaseModule } from '@data/database.module';
+import { getModelToken } from '@nestjs/mongoose';
+import { Order } from '@data/schema/order.schema';
+import { CourseHierarchy } from '@data/schema/courseHierarchy.schema';
+import { Model } from 'mongoose';
 
 describe('BackstageService', () => {
   let service: BackstageService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [DatabaseModule],
-      providers: [BackstageService],
+      providers: [
+        BackstageService,
+        {
+          provide: getModelToken(Order.name),
+          useValue: Model, // 使用 Mongoose 的 Model 作为 mock
+        },
+        {
+          provide: getModelToken(CourseHierarchy.name),
+          useValue: Model,
+        },
+      ],
     }).compile();
 
     service = module.get<BackstageService>(BackstageService);
--- src/api/fake-information/fake-information.service.spec.ts
+++ src/api/fake-information/fake-information.service.spec.ts
@@ -1,14 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FakeInformationService } from './fake-information.service';
-import { DatabaseModule } from '@data/database.module';
+import { getModelToken } from '@nestjs/mongoose';
+import { User } from '@data/schema/user.schema';
+import { CourseHierarchy } from '@data/schema/courseHierarchy.schema';
+import { PlatformCoupon } from '@data/schema/platformCoupon.schema';
+import { CourseTag } from '@data/schema/courseTag.schema';
+import { Model } from 'mongoose';
 
 describe('FakeInformationService', () => {
   let service: FakeInformationService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [DatabaseModule],
-      providers: [FakeInformationService],
+      providers: [
+        FakeInformationService,
+        {
+          provide: getModelToken(User.name),
+          useValue: Model,
+        },
+        {
+          provide: getModelToken(CourseHierarchy.name),
+          useValue: Model,
+        },
+        {
+          provide: getModelToken(PlatformCoupon.name),
+          useValue: Model,
+        },
+        {
+          provide: getModelToken(CourseTag.name),
+          useValue: Model,
+        },
+      ],
     }).compile();
 
     service = module.get<FakeInformationService>(FakeInformationService);
